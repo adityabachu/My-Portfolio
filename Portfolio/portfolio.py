52503b8afb8a645f3ca95cc7089b78d25b94f41d
Aditya Bachu <99969931+adityabachu@users.noreply.github.com>
2023-12-29 13:48:15 +0530

    Update portfolio.py

--- Portfolio/portfolio.py
+++ Portfolio/portfolio.py
@@ -23,6 +23,7 @@ def main():
             show_projects_section()
         with st.sidebar:
             components.html('<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="adityabachu" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/adityabachu?trk=profile-badge"></a></div>', height = 360 )
+            st.title("Coding Profiles")    
             st.title("[GitHub](https://github.com/adityabachu)")
             with open("Portfolio/Aditya Bachu Resume.pdf", "rb") as file:
                  btn = st.download_button(
