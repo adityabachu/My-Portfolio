e472ab3dfdf3f3f05c7055e6e35daaefd6cebc09
Aditya Bachu <99969931+adityabachu@users.noreply.github.com>
2024-01-07 12:21:05 +0530

    Update portfolio.py

--- Portfolio/portfolio.py
+++ Portfolio/portfolio.py
@@ -79,7 +79,7 @@ def show_contact_section():
 
     # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
     contact_form = """
-    <form action="https://formsubmit.co/YOUR@MAIL.COM" method="POST">
+    <form action="https://formsubmit.co/aditya.abhi03@gmail.com" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
